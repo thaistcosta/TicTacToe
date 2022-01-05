d3c5b211fe903c3196f4ff2df0ae4058696cc8a8
THAIS COSTA <thais_costa31@hotmail.com>
2022-01-05 06:50:42 +0000

    Testing the test ;)

--- src/application.js
+++ src/application.js
@@ -1,5 +1,7 @@
-const horizontalWin = (array) => {
-    return [...new Set(array)].length === 1;      
+const horizontalWin = (board) => {
+    let winningRow
+    board.forEach(array => [...new Set(array)].length === 1 && [...new Set(array)] !== [''] ? winningRow = [...new Set(array)] : null  )
+    return winningRow;      
 };
 
 
--- test/application.test.js
+++ test/application.test.js
@@ -4,7 +4,8 @@ import {
 
 describe('Application', function () {
   it('should know if a row is completed by the same player', () => {
-    expect(horizontalWin(['X','X','X'])).toBe(true);
+    const row = horizontalWin([['X','O','X'],['O','',''],['O','O','O']])    
+    expect(JSON.stringify(row) === JSON.stringify(['O'])).toBe(true);     // Should I make a test for each player ('X' and 'O')? or I should include the logical OR here
   });
 });
 
